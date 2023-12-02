876dd6a9d4ef713e72094da4aebf3df9dbe17d28
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-02 21:40:58 +0800

    create layout and input component

---
+++ src/components/Layout.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  type: String
+})
+</script>
+
+<template>
+  <template v-if="type === 'Guest'">
+    <main
+      class="flex flex-col min-h-screen justify-center items-center bg-light-background dark:bg-dark-background p-4"
+    >
+      <slot></slot>
+    </main>
+  </template>
+
+  <template v-else>
+    <main>
+      <slot></slot>
+    </main>
+  </template>
+</template>
+
+<script>
+export default {
+  name: 'AppLayout'
+}
+</script>
---
+++ src/components/forms/Input.vue
@@ -0,0 +1,37 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  label: String,
+  errorMessage: String,
+  isInvalid: Boolean
+})
+</script>
+
+<template>
+  <div>
+    <label :for="name" class="text-body-md text-light-neutral inline-block mb-2">{{ label }}</label>
+    <input
+      :id="name"
+      :name="name"
+      :type="type"
+      :class="{ 'border-danger': errorMessage }"
+      class="w-full rounded-[0.25rem] mb-1 focus:border-light-primary focus:ring-0"
+    />
+    <p class="text-danger text-body-md" :class="{ invisible: !errorMessage }">
+      {{ errorMessage ?? 'Invalid Message' }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormInput'
+}
+</script>
