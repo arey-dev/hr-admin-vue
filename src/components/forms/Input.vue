a8ae2563f07afcbbd530e153ef64563cb0523e5e
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-04 01:33:57 +0800

    add dark styles

--- src/components/forms/Input.vue
+++ src/components/forms/Input.vue
@@ -23,13 +23,13 @@ defineProps({
 
 <template>
   <div>
-    <label :for="name" class="text-body-md text-light-neutral inline-block mb-2">{{ label }}</label>
+    <label :for="name" class="text-body-md text-light-neutral inline-block mb-2 dark:text-dark-onSurface">{{ label }}</label>
     <input
       :id="name"
       :name="name"
       :type="type"
       :class="{ 'border-danger': errorMessage }"
-      class="w-full rounded-[0.25rem] mb-1 focus:border-light-primary focus:ring-0"
+      class="w-full text-light-onSurface rounded-[0.25rem] border-[#828fa3] border-opacity-25 mb-1   focus:border-light-primary focus:ring-0 dark:bg-dark-surface dark:text-dark-onSurface"
     />
     <p class="text-danger text-body-md" :class="{ invisible: !errorMessage }">
       {{ errorMessage ?? 'Invalid Message' }}
