e62251d9f3a8c4fd9b30c101f75af85c738fd4e4
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-04 01:33:34 +0800

    add button component

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,22 @@
 <script setup>
 import Input from '../components/forms/Input.vue'
+import Button from '../components/Button.vue'
 </script>
 
 <template>
-  <section class="flex gap-8 flex-col w-full bg-light-surface p-6 rounded-md">
-    <h1 class="tracking-wide text-heading-xl text-center">Welcome, Admin!</h1>
+  <section
+    class="flex gap-8 flex-col w-full bg-light-surface p-6 rounded-md shadow-md dark:bg-dark-surface"
+  >
+    <h1
+      class="tracking-wide text-heading-xl text-light-onSurface text-center dark:text-dark-onSurface"
+    >
+      Welcome, Admin!
+    </h1>
     <div class="flex flex-col gap-1">
       <Input label="Email" name="email" type="text" />
       <Input label="Password" name="password" type="password" />
     </div>
+    <Button variant="secondary"/>
   </section>
 </template>
 
