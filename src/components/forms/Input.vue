3882840cfb843615843d1a424e281e1c045d7f07
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-13 01:44:21 +0800

    refactor to accept v-model binding

--- src/components/forms/Input.vue
+++ src/components/forms/Input.vue
@@ -1,4 +1,5 @@
 <script setup>
+defineEmits(['update:modelValue'])
 defineProps({
   name: {
     type: String,
@@ -17,19 +18,28 @@ defineProps({
   },
   isInvalid: {
     type: Boolean
+  },
+  modelValue: {
+    type: String
   }
 })
 </script>
 
 <template>
   <div>
-    <label :for="name" class="text-body-md text-light-neutral inline-block mb-2 dark:text-dark-onSurface">{{ label }}</label>
+    <label
+      :for="name"
+      class="text-body-md text-light-neutral inline-block mb-2 dark:text-dark-onSurface"
+      >{{ label }}</label
+    >
     <input
       :id="name"
       :name="name"
       :type="type"
+      :value="modelValue"
       :class="{ 'border-danger': errorMessage }"
-      class="w-full text-light-onSurface rounded-[0.25rem] border-[#828fa3] border-opacity-25 mb-1   focus:border-light-primary focus:ring-0 dark:bg-dark-surface dark:text-dark-onSurface"
+      class="w-full text-light-onSurface rounded-[0.25rem] border-[#828fa3] border-opacity-25 mb-1 focus:border-light-primary focus:ring-0 dark:bg-dark-surface dark:text-dark-onSurface"
+      @input="$emit('update:modelValue', $event.target.value)"
     />
     <p class="text-danger text-body-md" :class="{ invisible: !errorMessage }">
       {{ errorMessage ?? 'Invalid Message' }}
