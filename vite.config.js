55bf03c13e2684e77358d08f7d426b52aa018136
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-15 01:31:04 +0800

    change port of the app

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  server: {
+    port: 8001
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
