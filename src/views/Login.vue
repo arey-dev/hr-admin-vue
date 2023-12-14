9f0893e8a71fc2e64219468f72dcb410b90b7127
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-15 01:35:46 +0800

    handle login of user

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,24 @@
 <script setup>
 import Input from '../components/forms/Input.vue'
 import Button from '../components/Button.vue'
+import { useAuthStore } from '../store/AuthStore'
+import { ref } from 'vue'
+
+const store = useAuthStore()
+
+const credentials = ref({
+  email: '',
+  password: ''
+})
+
+async function onSubmit() {
+  await store.login({ ...credentials.value })
+}
 </script>
 
 <template>
-  <section
+  <form
+    @submit.prevent="onSubmit"
     class="flex gap-8 flex-col w-full bg-light-surface p-6 rounded-md shadow-md dark:bg-dark-surface"
   >
     <h1
@@ -12,12 +26,14 @@ import Button from '../components/Button.vue'
     >
       Welcome, Admin!
     </h1>
-    <div class="flex flex-col gap-1">
-      <Input label="Email" name="email" type="text" />
-      <Input label="Password" name="password" type="password" />
+
+    <div class="flex flex-col gap-1 mb-4">
+      <Input label="Email" name="email" type="text" v-model="credentials.email" />
+      <Input label="Password" name="password" type="password" v-model="credentials.password" />
     </div>
-    <Button variant="secondary"/>
-  </section>
+
+    <Button variant="secondary" />
+  </form>
 </template>
 
 <script>
