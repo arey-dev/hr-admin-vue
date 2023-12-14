fccd778eb2dc21a75188cf0c316c6646c74a1ecb
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-15 01:35:10 +0800

    import axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import './axios'
 
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
