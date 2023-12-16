9f9fb7ea728a599867039a9b9ecf94af0b399efc
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-16 15:47:34 +0800

    craete avatar component

---
+++ src/components/Avatar.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div
+    class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600"
+  >
+    <span class="text-body-lg text-gray-600 dark:text-gray-300">RA</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserAvatar'
+}
+</script>
