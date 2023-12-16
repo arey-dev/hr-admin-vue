9cb15606bc44ba5b523cd73542e0a609f0c3966e
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-16 15:44:07 +0800

    add dashboard, employees, profile components

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
+import Dashboard from '../views/Dashboard.vue'
+import Employees from '../views/Employees.vue'
+import Profile from '../views/Profile.vue'
 import AuthLayout from '../components/layouts/AuthLayout.vue'
 import AppLayout from '../components/layouts/AppLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: AppLayout },
+    {
+      path: '/',
+      component: AppLayout,
+      children: [
+        { path: 'dashboard', component: Dashboard },
+        { path: 'employees', component: Employees },
+        { path: 'profile', component: Profile }
+      ]
+    },
     { path: '/auth', component: AuthLayout, children: [{ path: 'login', component: Login }] }
   ]
 })
