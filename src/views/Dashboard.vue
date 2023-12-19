dfe3e44be2c26fa63bb1792d250c1a200275fa6c
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-19 14:20:39 +0800

    add item requests card

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,9 +3,12 @@ import AnalyticsCard from '../components/AnalyticsCard.vue'
 </script>
 
 <template>
-  <div class="flex flex-col gap-2 py-2">
-    <AnalyticsCard title="Employees" data="10" icon="fa-solid fa-users" />
-    <AnalyticsCard title="Leaves" data="10" icon="fa-solid fa-person-walking-arrow-right" />
+  <div class="pt-4">
+    <section class="grid grid-cols-1 gap-4">
+      <AnalyticsCard title="Employees" data="10" icon="fa-solid fa-users" />
+      <AnalyticsCard title="Leaves" data="10" icon="fa-solid fa-person-walking-arrow-right" />
+      <AnalyticsCard title="Item Requests" data="10" icon="fa-solid fa-clipboard-list" />
+    </section>
   </div>
 </template>
 
