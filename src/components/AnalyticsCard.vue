a8e18dbfc6bfa0d5d7b388e76f37d2d50d05035b
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-19 14:18:28 +0800

    add dark styles

--- src/components/AnalyticsCard.vue
+++ src/components/AnalyticsCard.vue
@@ -16,9 +16,11 @@ defineProps({
 </script>
 
 <template>
-  <div class="flex items-center p-8 bg-white shadow rounded-lg">
+  <article
+    class="flex items-center p-8 bg-light-surface rounded-lg border border-light-lines dark:border-dark-lines dark:bg-dark-surface"
+  >
     <div
-      class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-purple-600 bg-purple-100 rounded-full mr-6"
+      class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-purple-600 bg-purple-100 rounded-full mr-6 dark:bg-dark-onSurface"
     >
       <font-awesome-icon
         :icon="icon"
@@ -27,8 +29,8 @@ defineProps({
       />
     </div>
     <div>
-      <span class="block text-heading-xl">{{ data }}</span>
-      <span class="block text-light-neutral">{{ title }}</span>
+      <h2 class="block text-light-neutral dark:text-dark-neutral">{{ title }}</h2>
+      <p class="text-heading-xl text-light-onSurface dark:text-dark-onSurface">{{ data }}</p>
     </div>
-  </div>
+  </article>
 </template>
