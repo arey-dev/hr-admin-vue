4511e23fa84bcd143ffb8ec06c0b89c8602e9349
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-21 04:38:13 +0800

    create badge indicator for status column

---
+++ src/components/StatusIndicator.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  status: {
+    type: String,
+    default: 'Active'
+  }
+})
+</script>
+
+<template>
+  <span
+    :class="[
+      status === 'Active'
+        ? 'text-green-800 bg-green-100 dark:bg-green-900 dark:text-green-300'
+        : 'text-red-800 bg-red-100 dark:bg-red-900 dark:text-red-300'
+    ]"
+    class="inline-flex items-center text-xs font-medium px-2.5 py-0.5 rounded-full"
+  >
+    <span
+      :class="[status === 'Active' ? 'bg-green-500' : 'bg-red-500']"
+      class="w-2 h-2 me-1 rounded-full"
+    ></span>
+    {{ status }}
+  </span>
+</template>
