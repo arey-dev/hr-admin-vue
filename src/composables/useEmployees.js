ff6e5baff1ad374f310f5b270d95d550e05b8c0c
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-26 17:01:22 +0800

    refactor: add a function to fetch a specific page in pagination, seperate emplyee data and page info

--- src/composables/useEmployees.js
+++ src/composables/useEmployees.js
@@ -3,13 +3,38 @@ import { ref } from 'vue'
 
 export function useEmployees() {
   const employees = ref(null)
+  const pageInfo = ref(null)
   const errors = ref(null)
+  const isLoading = ref(false)
 
   const getEmployees = async () => {
+    isLoading.value = true
+
     const response = await axios.get('/employees')
 
-    employees.value = response.data.data
+    // seperate data that contains employee info
+    // and data thaat contains page info
+    const { data, ...rest } = response.data.data
+
+    employees.value = data
+    pageInfo.value = rest
+
+    isLoading.value = false
   }
 
-  return { employees, errors, getEmployees }
+  const getPage = async (page) => {
+    isLoading.value = true
+    const response = await axios.get(`/employees?page=${page}`)
+
+    // seperate data that contains employee info
+    // and data thaat contains page info
+    const { data, ...rest } = response.data.data
+
+    employees.value = data
+    pageInfo.value = rest
+
+    isLoading.value = false
+  }
+
+  return { employees, errors, isLoading, pageInfo, getEmployees, getPage }
 }
