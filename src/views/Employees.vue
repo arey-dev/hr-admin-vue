8abe6193986c56d6a881530e3d60a24f5e561f97
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-26 17:02:17 +0800

    use pagination and show spinner when data is loading

--- src/views/Employees.vue
+++ src/views/Employees.vue
@@ -6,8 +6,14 @@ import Search from '../components/forms/Search.vue'
 import TableFilter from '../components/TableFilter.vue'
 import Avatar from '../components/Avatar.vue'
 import StatusIndicator from '../components/StatusIndicator.vue'
+import TablePagination from '../components/TablePagination.vue'
+import Spinner from '../components/Spinner.vue'
 
-const { employees, getEmployees } = useEmployees()
+const { employees, isLoading, pageInfo, getEmployees, getPage } = useEmployees()
+
+function changePage(page) {
+  getPage(page)
+}
 
 onMounted(() => getEmployees())
 </script>
@@ -20,102 +26,114 @@ onMounted(() => getEmployees())
         <TableFilter />
       </div>
 
-      <div class="relative overflow-x-auto shadow-md rounded-lg">
-        <table
-          class="w-full text-body-lg text-left rtl:text-right text-gray-500 dark:text-gray-400"
-        >
-          <thead
-            class="text-heading-sm text-light-neutral uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+      <div class="relative overflow-x-auto overflow-y-visible shadow-md rounded-lg">
+        <div v-if="isLoading" class="py-4">
+          <Spinner />
+        </div>
+        <template v-else>
+          <table
+            class="w-full text-body-lg text-left rtl:text-right text-gray-500 dark:text-gray-400"
           >
-            <tr>
-              <th scope="col" class="px-6 py-3">Id</th>
-              <th scope="col" class="px-6 py-3">
-                <div class="flex items-center">
-                  Employee
-                  <a href="#"
-                    ><font-awesome-icon
-                      icon="fa-solid fa-sort"
-                      class="w-2 h-3 ms-2 text-light-onSurface dark:text-dark-onPrimary"
-                      aria-hidden="true"
-                    />
-                  </a>
-                </div>
-              </th>
-              <th scope="col" class="px-6 py-3">
-                <div class="flex items-center">
-                  Employed
-                  <a href="#"
-                    ><font-awesome-icon
-                      icon="fa-solid fa-sort"
-                      class="w-2 h-3 ms-2 text-light-onSurface dark:text-dark-onPrimary"
-                      aria-hidden="true"
-                    />
-                  </a>
-                </div>
-              </th>
-              <th scope="col" class="px-6 py-3">
-                <div class="flex items-center">
-                  Job
-                  <a href="#"
-                    ><font-awesome-icon
-                      icon="fa-solid fa-sort"
-                      class="w-2 h-3 ms-2 text-light-onSurface dark:text-dark-onPrimary"
-                      aria-hidden="true"
-                    />
-                  </a>
-                </div>
-              </th>
-              <th scope="col" class="px-6 py-3">
-                <div class="flex items-center">
-                  Status
-                  <a href="#"
-                    ><font-awesome-icon
-                      icon="fa-solid fa-sort"
-                      class="w-2 h-3 ms-2 text-light-onSurface dark:text-dark-onPrimary"
-                      aria-hidden="true"
-                    />
-                  </a>
-                </div>
-              </th>
-              <th scope="col" class="px-6 py-3">
-                <span class="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="employee in employees"
-              :key="employee.id"
-              class="bg-light-surface border-b border-light-lines dark:bg-dark-surface dark:border-dark-lines"
+            <thead
+              class="text-heading-sm text-light-neutral uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
             >
-              <th
-                scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                {{ employee.id }}
-              </th>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="flex gap-4 items-center">
-                  <Avatar :f_name="employee.f_name" :l_name="employee.l_name" />
-                  <div>
-                    <p>{{ employee.f_name + ' ' + employee.l_name }}</p>
-                    <p>{{ employee.email }}</p>
+              <tr>
+                <th scope="col" class="px-6 py-3">Id</th>
+                <th scope="col" class="px-6 py-3">
+                  <div class="flex items-center">
+                    Employee
+                    <a href="#"
+                      ><font-awesome-icon
+                        icon="fa-solid fa-sort"
+                        class="w-2 h-3 ms-2 text-light-onSurface dark:text-dark-onPrimary"
+                        aria-hidden="true"
+                      />
+                    </a>
                   </div>
-                </div>
-              </td>
-              <td class="px-6 py-4">{{ formatDate(employee.employment_start) }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <p>{{ employee.job_title }}</p>
-                <p>{{ employee.job_level }}</p>
-              </td>
-              <td class="px-6 py-4"><StatusIndicator :status="employee.status" /></td>
-              <td class="px-6 py-4 text-right">
-                <font-awesome-icon icon="fa-solid fa-eye" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  <div class="flex items-center">
+                    Employed
+                    <a href="#"
+                      ><font-awesome-icon
+                        icon="fa-solid fa-sort"
+                        class="w-2 h-3 ms-2 text-light-onSurface dark:text-dark-onPrimary"
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </div>
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  <div class="flex items-center">
+                    Job
+                    <a href="#"
+                      ><font-awesome-icon
+                        icon="fa-solid fa-sort"
+                        class="w-2 h-3 ms-2 text-light-onSurface dark:text-dark-onPrimary"
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </div>
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  <div class="flex items-center">
+                    Status
+                    <a href="#"
+                      ><font-awesome-icon
+                        icon="fa-solid fa-sort"
+                        class="w-2 h-3 ms-2 text-light-onSurface dark:text-dark-onPrimary"
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </div>
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  <span class="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="employee in employees"
+                :key="employee.id"
+                class="bg-light-surface border-b border-light-lines dark:bg-dark-surface dark:border-dark-lines"
+              >
+                <th
+                  scope="row"
+                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {{ employee.id }}
+                </th>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="flex gap-4 items-center">
+                    <Avatar :f_name="employee.f_name" :l_name="employee.l_name" />
+                    <div>
+                      <p>{{ employee.f_name + ' ' + employee.l_name }}</p>
+                      <p>{{ employee.email }}</p>
+                    </div>
+                  </div>
+                </td>
+                <td class="px-6 py-4">{{ formatDate(employee.employment_start) }}</td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <p>{{ employee.job_title }}</p>
+                  <p>{{ employee.job_level }}</p>
+                </td>
+                <td class="px-6 py-4"><StatusIndicator :status="employee.status" /></td>
+                <td class="px-6 py-4 text-right">
+                  <font-awesome-icon icon="fa-solid fa-eye" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </template>
       </div>
+
+      <TablePagination
+        :current-page="pageInfo?.current_page"
+        :total-pages="pageInfo?.last_page"
+        @on-next-page="changePage"
+        @on-previous-page="changePage"
+      />
     </section>
   </div>
 </template>
