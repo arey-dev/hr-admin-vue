b738a7a121debe5ecc1229906f9a9271315b82d8
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-26 17:01:50 +0800

    create a pagination component

---
+++ src/components/TablePagination.vue
@@ -0,0 +1,49 @@
+<script setup>
+import Button from './Button.vue'
+import { computed } from 'vue'
+
+const props = defineProps({
+  currentPage: {
+    validator(value) {
+      return typeof value === 'number' || value === 'null'
+    }
+  },
+  totalPages: {
+    validator(value) {
+      return typeof value === 'number' || value === 'null'
+    }
+  }
+})
+
+const isLastPage = computed(() => {
+  return props.currentPage === props.totalPages
+})
+
+const isFirstPage = computed(() => {
+  return props.currentPage === 1
+})
+</script>
+
+<template>
+  <div class="flex justify-between items-center">
+    <Button
+      @click="$emit('onPreviousPage', currentPage - 1)"
+      variant="secondary"
+      :disabled="isFirstPage"
+      class="w-28"
+    >
+      <font-awesome-icon icon="fa-solid fa-arrow-left" class="me-2" />
+      Previous
+    </Button>
+    <span class="text-body-lg text-light-primary dark:text-dark-primary">{{ currentPage }}</span>
+    <Button
+      @click="$emit('onNextPage', currentPage + 1)"
+      variant="secondary"
+      :disabled="isLastPage"
+      class="w-28"
+    >
+      Next
+      <font-awesome-icon icon="fa-solid fa-arrow-right" class="ms-2" />
+    </Button>
+  </div>
+</template>
