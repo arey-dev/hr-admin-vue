d531063fc76a8e990aeea1ad9c5ab77e1c7904ab
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-27 19:20:48 +0800

    create composable for login function

---
+++ src/composables/useAuth.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import { ref, shallowRef } from 'vue'
+import { useAuthUserStore } from '../store/AuthUserStore'
+
+export function useAuth() {
+  const store = useAuthUserStore()
+  const isLoading = ref(false)
+  const errors = shallowRef(null)
+
+  async function login(credentials) {
+    isLoading.value = true
+
+    try {
+      const { data } = await axios.post('/auth/login', credentials)
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${data.access_token}`
+
+      store.setUser(data.user)
+    } catch (error) {
+      if (error.response.status === 401) {
+        errors.value = error.response.data.errors
+      }
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  return { isLoading, errors, login }
+}
