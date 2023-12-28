e6dfd0ac1e24745c932d5c96cd9761cf9ed1e3ac
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-28 13:43:20 +0800

    add navigation guards to routes that needs authentication

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Employees from '../views/Employees.vue'
 import Profile from '../views/Profile.vue'
 import AuthLayout from '../components/layouts/AuthLayout.vue'
 import AppLayout from '../components/layouts/AppLayout.vue'
+import { useAuthUserStore } from '../store/AuthUserStore'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,14 +14,45 @@ const router = createRouter({
       path: '/',
       component: AppLayout,
       redirect: '/dashboard',
+      meta: {
+        requiresAuth: true
+      },
       children: [
         { path: 'dashboard', name: 'Dashboard', component: Dashboard },
         { path: 'employees', name: 'Employees', component: Employees },
         { path: 'profile', name: 'Profile', component: Profile }
       ]
     },
-    { path: '/auth', component: AuthLayout, children: [{ path: 'login', component: Login }] }
+    {
+      path: '/auth',
+      component: AuthLayout,
+      meta: {
+        requiresAuth: false
+      },
+      children: [
+        {
+          path: 'login',
+          component: Login
+        }
+      ]
+    }
   ]
+})
+
+// eslint-disable-next-line no-unused-vars
+router.beforeEach((to, from) => {
+  const auth = useAuthUserStore()
+
+  // if route requires auth and,
+  // and there is no authenticated user
+  if (to.meta.requiresAuth && !auth.user) {
+    // redirect to login
+    return {
+      path: '/auth/login',
+      // save the location we were at to come back later
+      query: { redirect: to.fullPath }
+    }
+  }
 })
 
 export default router
