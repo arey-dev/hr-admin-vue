582b20dbcb75840c9c3b67f1c8f418580569e370
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-31 01:51:48 +0800

    watch filters, fetch data on every filter added

--- src/composables/useEmployees.js
+++ src/composables/useEmployees.js
@@ -7,20 +7,42 @@ export function useEmployees() {
   const errors = ref(null)
   const isLoading = ref(false)
 
-  const getEmployees = async (query, page = 1) => {
+  const getEmployees = async (search, filters, page = 1) => {
     isLoading.value = true
 
-    const url = query ? `/employees?q=${query}&page=${page}` : `/employees?&page=${page}`
+    const query = []
+
+    if (search) {
+      query.push('q=' + search)
+    }
+
+    if (filters) {
+      for (const { field, value } of filters) {
+        console.log(field, value)
+        query.push(field + '=' + value)
+      }
+    }
+
+    if (page) {
+      query.push('page=' + page)
+    }
+
+    // 'field=value'
+    const queryString = query.join('&')
+
+    console.log(queryString)
+
+    const url = `/employees?${query.length === 1 ? query[0] : queryString}`
 
     const response = await axios.get(url)
 
     // seperate data that contains employee info
-    // and data thaat contains page info
+    // and data that contains page info
     const { data, ...rest } = response.data
 
     employees.value = data
 
-    // pagination info are store here,
+    // pagination info are stored here,
     // currentPage, last page link, first page link, etc.
     pageInfo.value = rest
 
