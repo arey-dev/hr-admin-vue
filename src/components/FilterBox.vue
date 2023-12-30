7315955ccc64d133e2c96eb8be43a2bdab028536
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-30 18:00:55 +0800

    create a container for filters

---
+++ src/components/FilterBox.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Chip from './Chip.vue'
+
+defineProps({
+  filters: {
+    type: Set,
+    required: true
+  },
+  onRemove: {
+    type: Function
+  }
+})
+</script>
+
+<template>
+  <div class="flex flex-wrap gap-2">
+    <Chip v-for="filter in filters" :key="filter" :label="filter" removable @on-remove="onRemove">
+      {{ filter }}
+    </Chip>
+  </div>
+</template>
