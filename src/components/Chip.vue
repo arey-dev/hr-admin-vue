4785d0581e0cee8c6d4deb79acb00966eb3e0a17
R.A Borja <borja.ronaldandrew@gmail.com>
2023-12-30 18:00:12 +0800

    create chip component to present filter

---
+++ src/components/Chip.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String
+  },
+  removable: {
+    type: Boolean
+  }
+})
+</script>
+
+<template>
+  <div class="inline-flex bg-light-primary text-white text-body-lg rounded-full py-1 px-2 gap-2">
+    <font-awesome-icon v-if="icon" :icon="icon" />
+    <span>
+      {{ label }}
+    </span>
+    <button v-if="removable" @click="$emit('onRemove', label)">
+      <font-awesome-icon icon="fa-solid fa-circle-xmark" />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppChip'
+}
+</script>
